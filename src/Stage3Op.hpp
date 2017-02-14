ccc66ca329f8273b7e71845bb5838d7793629c9a
Brandon Fikes <bpfikes@gmail.com>
2017-02-13 19:42:38 -0600

    Added Stage 3

---
+++ src/Stage3Op.hpp
@@ -0,0 +1,22 @@
+#ifndef STAGE3OP_HPP
+#define STAGE3OP_HPP
+
+#include "gpio.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <time.h>
+
+/***************************************************
+ * Stage3OP.hpp - An IEEE SECON 2017 Header File   *
+ * Author(s): Brandon Fikes                        *
+ * Intent: Drive the stepper motor for Stage 3 by  *
+ * taking in 5 integer values corresponding to the *
+ * codes obtained from Stage 1 of the competition  *
+ **************************************************/
+
+void Stage3Op(int c1, int c2, int c3, int c4, int c5);
+
+#endif
+
