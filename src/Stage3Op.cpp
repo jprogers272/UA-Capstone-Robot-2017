caebf1b27ea3f8248c2fb44743f207e6e3a63e61
Brandon Fikes <bpfikes@gmail.com>
2017-02-14 18:27:35 -0600

    Updated readability / fixed bugs

--- src/Stage3Op.cpp
+++ src/Stage3Op.cpp
@@ -24,23 +24,22 @@
   ***********************************************/
 
 
+#define DELAY_Motor 5 //5 milliseconds
+#define DELAY_NEXT  2 //2 seconds
+
+//robotWait is used as follows: robotWait(seconds, milliseconds)
+
 
 void Stage3Op(int c1, int c2, int c3, int c4, int c5){
 
 	int i; //loop variable
 
-	//delay structuring
-	struct timespec time_des, time_rem;
-	time_des.tv_sec = 0;
-	time_des.tv_nsec = 10000000; //10ms
-	time_rem.tv_nsec = 0; //not sure what this is doing
-
-
-
+	//configure pins used
 	setDirectionGPIO(45, 0); //STEP = GPIO PIN 45 (OUTPUT)
 	setDirectionGPIO(44, 0); //DIR  = GPIO PIN 44 (OUTPUT)
 	setDirectionGPIO(26, 0); //ENABLE = GPIO PIN 26 (OUTPUT)
 
+	//wake the stepper driver from sleep
 	writeGPIO(26, 1);
 
 	//First code, c1, is to be turned c1 times clockwise
@@ -48,55 +47,65 @@ void Stage3Op(int c1, int c2, int c3, int c4, int c5){
 	for (i = 0; i < 200*c1; i++){
 		writeGPIO(45, 1); //STEP
 		//delay
-		nanosleep(&time_des, &time_rem);
+		robotWait(0, DELAY_MOTOR);
 		writeGPIO(45, 0);  //!STEP
 	}
-	robotWait(2, 0);
+
+	//settle
+	robotWait(DELAY_NEXT, 0);
+
 	//Second code, c2, is to be turned c2 times counter-clockwise
 	writeGPIO(44, 1); //DIR = ROTATE COUNTER-CLOCKWISE
 	for (i = 0; i < 200*c2; i++){
 		writeGPIO(45, 1); //STEP
 		//delay
-		nanosleep(&time_des, &time_rem);
+		robotWait(0, DELAY_MOTOR);
 		writeGPIO(45, 0);  //!STEP
 	}
 
-	robotWait(2, 0);
+	//settle
+	robotWait(DELAY_NEXT, 0);
 
 	//Third code, c3, is to be turned c3 times clockwise
 	writeGPIO(44, 0); //DIR = ROTATE CLOCKWISE
 	for (i = 0; i < 200*c3; i++){
 		writeGPIO(45, 1); //STEP
 		//delay
-		nanosleep(&time_des, &time_rem);
+		robotWait(0, DELAY_MOTOR);
 		writeGPIO(45, 0);  //!STEP
 	}
 	
-	robotWait(2, 0);
+	//settle
+	robotWait(DELAY_NEXT, 0);
+
 	//Fourth code, c4, is to be turned c4 times counter-clockwise
 	writeGPIO(44, 1); //DIR = ROTATE COUNTER-CLOCKWISE
 	for (i = 0; i < 200*c4; i++){
 		writeGPIO(45, 1); //STEP
 		//delay
-		nanosleep(&time_des, &time_rem);
+		robotWait(0, DELAY_MOTOR);
 		writeGPIO(45, 0);  //!STEP
 	}
 
-	robotWait(2, 0);
+	//settle
+	robotWait(DELAY_NEXT, 0);
 
 	//Fifth code, c5, is to be turned c5 times clockwise
 	writeGPIO(44, 0); //DIR = ROTATE COUNTER-CLOCKWISE
 	for (i = 0; i < 200*c5; i++){
 		writeGPIO(45, 1); //STEP
 		//delay
-		nanosleep(&time_des, &time_rem);
+		robotWait(0, DELAY_MOTOR);
 		writeGPIO(45, 0);  //!STEP
 	}
 	
-	writeGPIO(26, 0); //disable board
+	//settle
+	robotWait(DELAY_NEXT, 0);
+
+	//put the stepper driver to sleep
+	writeGPIO(26, 0);
 	return;
 
-	robotWait(2, 0);
 
 }
 
